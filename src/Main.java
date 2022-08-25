0871c72451a7bb6b026b35f72894a5fd26d989a8
Вячеслав <friendcw@mail.ru>
2022-08-25 22:28:22 +0400

    Задачи 4-5

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
-        //Задача 1
-        System.out.println("Задача 1");
-        var dog = 8.0;
-        System.out.println(dog);
-        var cat = 3.6;
-        System.out.println(cat);
-        var paper = 763789;
-        System.out.println(paper);
+        //Задача 4
+        System.out.println("Задача 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend += 2;
+        System.out.println(friend);
+        friend /= 7;
+        System.out.println(friend);
 
-        //Задача 2
-        System.out.println("Задача 2");
-        dog = dog + 4;
-        System.out.println(dog);
-        cat = 3.6 + 4;
-        System.out.println(cat);
-        paper = 763789 + 4;
-        System.out.println(paper);
+        //Задача 5
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog *= 10;
+        System.out.println(frog);
+        frog /= 3.5;
+        System.out.println(frog);
+        frog += 4;
+        System.out.println(frog);
 
-        //Задача 3
-        System.out.println("Задача 3");
-        dog = dog - 3.5;
-        System.out.println(dog);
-        cat = cat - 1.6;
-        System.out.println(cat);
-        paper = paper - 7639;
-        System.out.println(paper);
     }
 }
